6c93bc8cea459f92ca20d14988ea6a21112685e1
Artsiom <prohorenkoartem111@gmail.com>
2023-08-20 14:57:24 +0200

    Create model

--- Referral/main/models.py
+++ Referral/main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    phone_number = models.CharField(unique=True, max_length=20)
+    name = models.CharField(max_length=255)  # Добавьте максимальную длину для имени
+    otp = models.CharField(max_length=4, null=True, blank=True)
+    invite_code = models.CharField(max_length=6, null=True, blank=True)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['name']
+
+    objects = UserManager()
+
+    def full_name(self):  # Измените имя метода, чтобы избежать конфликта с полем "name"
+        return self.name + ' ' + self.phone_number
+
+    def __str__(self):
+        return self.phone_number
