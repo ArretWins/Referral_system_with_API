4bbfb4236a41881e12115c52c326960a327c6f2a
Artsiom <prohorenkoartem111@gmail.com>
2023-08-22 17:31:07 +0200

    true otp with html

--- Referral/Referral/urls.py
+++ Referral/Referral/urls.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
-from main.views import OTPVerificationView, PhoneAuthorizationView
+from main.views import PhoneAuthorizationView, PhoneLoginView, UserViewSet
+
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('authorize-phone/', PhoneAuthorizationView.as_view(), name='phone-authorization'),
-    path('verify-otp/', OTPVerificationView.as_view(), name='otp-verification'),
+    path('send-otp/', PhoneAuthorizationView.as_view(), name='send-otp'),
+    path('login/', PhoneLoginView.as_view(), name='login'),
+    path('', include(router.urls)),  # Включаем роутер URL-путей
 ]
 
--- Referral/main/views.py
+++ Referral/main/views.py
@@ -1,28 +1,23 @@
+import random
+import string
+
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from twilio.rest import Client
 
 
 from .models import User
-from .serializers import UserSerializer
-from .utils import generate_otp, send_otp_sms
 from Referral.settings import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN
+from .serializers import UserSerializer
 
 verify_sid = "VA7f6ae1a5cc074e5fcbb49f40cbd7d553"
 
-
-# class UserRegistrationView(generics.CreateAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class PhoneAuthorizationView(APIView):
@@ -31,47 +26,58 @@ class PhoneAuthorizationView(APIView):
 
     def post(self, request):
         phone_number = request.data.get('phone_number')
-        if phone_number:
-            user, created = User.objects.get_or_create(phone_number=phone_number)
 
-            if created or not user.is_verified:
-                otp = generate_otp()  # Используйте вашу функцию генерации OTP
-                user.otp = otp
-                user.save()
-
-                send_otp_sms(phone_number, otp)  # Отправьте SMS с кодом авторизации
-
-                return Response({'message': 'OTP has been sent to your phone number.'}, status=status.HTTP_200_OK)
-            else:
-                return Response({'message': 'User already verified.'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
+        if not phone_number:
             return Response({'message': 'Phone number is required.'}, status=status.HTTP_400_BAD_REQUEST)
 
+        # Generate 4-digit OTP
+        # otp = ''.join(random.choices(string.digits, k=4))
 
-class OTPVerificationView(APIView):
+        # Send OTP
+        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        message = client.verify.services(verify_sid) \
+            .verifications \
+            .create(to=phone_number, channel="sms")
+
+        try:
+            user = User.objects.get(phone_number=phone_number)
+            # Если пользователь уже авторизован, то не меняем инвайт-код
+            if not user:
+                # Generate random invite code
+                invite_code = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+                user.invite_code = invite_code
+                user.save()
+        except User.DoesNotExist:
+            # Generate random invite code for a new user
+            invite_code = ''.join(random.choices(string.ascii_letters + string.digits, k=6))
+            user = User.objects.create(phone_number=phone_number, invite_code=invite_code)
+
+        return Response({'message': 'OTP has been sent and invite code generated.'}, status=status.HTTP_200_OK)
+
+
+class PhoneLoginView(APIView):
+
     def get(self, request):
-        return render(request, 'main/otp_verification.html')
+        return render(request, 'main/otp_login.html')
 
     def post(self, request):
         phone_number = request.data.get('phone_number')
         otp = request.data.get('otp')
 
-        if phone_number and otp:
+        if not phone_number or not otp:
+            return Response({'message': 'Phone number and OTP are required.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+        verification = client.verify.services(verify_sid) \
+            .verification_checks \
+            .create(to=phone_number, code=otp)
+
+        if verification.status == 'approved':
             try:
                 user = User.objects.get(phone_number=phone_number)
-                client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-                # В этом месте нам не нужно проверять otp, потому что теперь Twilio Verify Service заботится об этом
-                verification = client.verify \
-                    .services(verify_sid) \
-                    .verification_checks \
-                    .create(to=phone_number, code=otp)
-                if verification.status == 'approved':
-                    user.is_verified = True
-                    user.save()
-                    return Response({'message': 'User has been verified successfully.'}, status=status.HTTP_200_OK)
-                else:
-                    return Response({'message': 'Invalid phone number or OTP.'}, status=status.HTTP_400_BAD_REQUEST)
+                login(request, user)
+                return Response({'message': 'User logged in successfully.'}, status=status.HTTP_200_OK)
             except User.DoesNotExist:
                 return Response({'message': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
         else:
-            return Response({'message': 'Phone number and OTP are required.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': 'Invalid phone number or OTP.'}, status=status.HTTP_401_UNAUTHORIZED)
