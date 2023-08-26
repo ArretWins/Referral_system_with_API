bcd8bc9542ce54eac95a4c666c1b2b595ad84908
Artsiom <prohorenkoartem111@gmail.com>
2023-08-26 13:04:01 +0200

    outher_invite_code field

--- Referral/main/models.py
+++ Referral/main/models.py
@@ -5,13 +5,16 @@ from .manager import UserManager
 
 class User(AbstractUser):
     username = None
+    first_name = None
+    last_name = None
+    email = None
     phone_number = models.CharField(unique=True, max_length=20)
     name = models.CharField(max_length=255)  # Добавьте максимальную длину для имени
     # otp = models.CharField(max_length=4, null=True, blank=True)
     invite_code = models.CharField(max_length=6, null=True, blank=True)
     # is_verified = models.BooleanField(default=False)
     invite_code_activated = models.BooleanField(default=False)
-
+    outher_invite_code = models.CharField(max_length=6, null=True, blank=True)
     USERNAME_FIELD = 'phone_number'
     REQUIRED_FIELDS = ['name']
 
