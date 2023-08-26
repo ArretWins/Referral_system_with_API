b610b5a9932183e9b6ef394cde89aa9ed191e585
Artsiom <prohorenkoartem111@gmail.com>
2023-08-26 13:05:04 +0200

    delete comments

--- Referral/Referral/urls.py
+++ Referral/Referral/urls.py
@@ -7,7 +7,6 @@ from main.views import PhoneAuthorizationView, PhoneLoginView, UserViewSet, User
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
-# router.register(r'users', UserProfileViewSet)
 router.register(r'user-profile', UserProfileViewSet, basename='user-profile')
 
 
@@ -17,8 +16,7 @@ urlpatterns = [
     path('login/', PhoneLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('profile/', UserProfileViewSet.as_view({'post': 'activate_invite_code'}), name='profile'),
-    path('', include(router.urls)),  # Включаем роутер URL-путей
+    path('', include(router.urls)),
     path('api/', include(router.urls)),
-    # path('activate-invite-code/', UserProfileViewSet.as_view({'post': 'activate_invite_code'}), name='activate_invite_code'),
 ]
 
